3852aba46626fd78af8759938ba8a952e5a035fe
GamanMil <daboy1569@gmail.com>
2026-04-29 19:20:57 +0100

    added mainmenu ui and options to play with others or ai (not implemented)

---
+++ Scripts/GameState.gd
@@ -0,0 +1,6 @@
+extends Node
+
+const PLAY_VS_AI = 0
+const PLAY_WITH_PEOPLE = 1
+
+var play_mode = PLAY_VS_AI
---
+++ Scripts/MainMenu.gd
@@ -0,0 +1,9 @@
+extends Control
+
+func _on_button_pressed() -> void:
+	GameState.play_mode = GameState.PLAY_VS_AI
+	get_tree().change_scene_to_file("res://Scenes/test_board.tscn")
+
+func _on_button_2_pressed() -> void:
+	GameState.play_mode = GameState.PLAY_WITH_PEOPLE
+	get_tree().change_scene_to_file("res://Scenes/test_board.tscn")
